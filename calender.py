915c7b486e539f0f74c04db7df1a785ef3f3aaa0
JeonJongYook <jggen0121@naver.com>
2022-03-30 18:15:35 +0900

    2022-03-30

---
+++ calender.py
@@ -0,0 +1,12 @@
+import calendar
+print("")
+print("")
+print(calendar.month(2022,1))   # 2022년도 1월
+print("")
+print("")
+print(calendar.month(2022, 2))   # 2022년도 2월
+print("")
+print("")
+print(calendar.month(2022, 3))   # 2022년도 3월
+print("")
+
---
+++ turtle_test.py
@@ -0,0 +1,14 @@
+import turtle as t
+
+t.shape("turtle")
+t.forward(90)
+t.right(144)
+t.forward(90)
+t.right(144)
+t.forward(90)
+t.right(144)
+t.forward(90)
+t.right(144)
+t.forward(90)
+t.left(144)
+t.circle(70, 360)
